a38138f91771e4421d2a948987e4f9e56a226095
arinik9 <arinik9@hotmail.com>
2017-08-01 23:02:47 +0200

    fix jar file name

--- src/define-consts.R
+++ src/define-consts.R
@@ -6,7 +6,7 @@
 
 IM = "infomap"
 ExCC = "ExCC"
-ExCC.JAR.PATH = "jar/cplex-partition.jar"
+ExCC.JAR.PATH = "jar/cplex-partition-openjdk-1_8.jar"
 # where the .so files are located
 CPLEX.BIN.PATH = "/opt/ibm/ILOG/CPLEX_Studio1271/cplex/bin/x86-64_linux/" #  path for Nejat's computer
 # CPLEX.BIN.PATH = "/usr/local/ibm/ILOG/CPLEX_Studio1261/cplex/bin/x86-64_linux/"# 'path for "gaia" cluster
